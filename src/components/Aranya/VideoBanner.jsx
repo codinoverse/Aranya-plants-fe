cd2b9910965b69357d08a8f1c2221667a101e38b
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2025-07-10 19:10:08 +0530

    updated header

--- src/components/Aranya/VideoBanner.jsx
+++ src/components/Aranya/VideoBanner.jsx
@@ -1,17 +1,15 @@
-
 import './VideoBanner.css';
 
 const VideoBanner = () => {
   return (
     <div className="video-banner position-relative text-white">
       <video className="w-100 h-100 object-fit-cover" autoPlay muted loop playsInline>
-        <source src="./public/videos/video3.mp4" type="video/mp4" />
+        <source src="/videos/video3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      {/* Overlay content */}
-      <div className="banner-content position-absolute top-50  start-50 translate-middle text-center">
-        <h1 className="fw-bold banner-heading">Aranya Plants & Pots</h1> 
+      <div className="banner-content position-absolute top-50 start-50 translate-middle text-center">
+        <h1 className="fw-bold banner-heading">Aranya Plants & Pots</h1>
         <p className="banner-subtext">The best season to start afresh.</p>
       </div>
     </div>
